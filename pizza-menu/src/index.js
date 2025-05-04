dd9ae681d7aab84e4f2fc6c4d9ad9e6d97d95044
dimagutorka <dgutorka@gmail.com>
2025-05-04 17:04:38 +0300

    Connect css and style some components

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
     {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
     return (
-        <div>
+        <div className="container">
             <Header />
             <Menu />
             <Footer />
@@ -57,24 +58,45 @@ function App() {
 }
 
 function Header() {
-    return <h1>Fast React pizza Co.</h1>;
+    // const inlineStyles = {
+    //             color: "red",
+    //             fontSize: "32px",
+    //             textTransform: "uppercase",
+    //         }
+
+    const inlineStyles = {};
+
+    return (
+        <header className="header">
+            <h1 style={inlineStyles}>Fast React pizza Co.</h1>
+        </header>
+    );
 }
 
 function Menu() {
     return (
-        <div>
+        <main className="menu">
             <h2>Our menu</h2>
             <Pizza />
             <Pizza />
             <Pizza />
             <Pizza />
-        </div>
+        </main>
     );
 }
 
 function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+
+    console.log(isOpen);
+
     return (
-        <footer>{new Date().toLocaleTimeString()} We're currently open</footer>
+        <footer className="footer">
+            {new Date().toLocaleTimeString()} We're currently open
+        </footer>
     );
 
     // return React.createElement("footer", null, "We are currently open");
@@ -84,7 +106,7 @@ function Pizza() {
     return (
         <div>
             <img src="./pizzas/spinaci.jpg" alt="Pizza spinaci" />
-            <h2>Pizza Prosciutto</h2>
+            <h3>Pizza Prosciutto</h3>
             <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
         </div>
     );
