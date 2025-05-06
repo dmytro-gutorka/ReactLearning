c3f4388d22a81253aa3453344407319a15aad88e
dimagutorka <dgutorka@gmail.com>
2025-05-06 20:11:37 +0300

    Flash-cards projects

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -67,9 +67,7 @@ function PackingList() {
   return (
      <div className="list">
          <ul>
-             {initialItems.map(item => (
-                 <Item item={item} key={item.id}/>
-             ))}
+             {initialItems.map(item => (<Item item={item} key={item.id}/>))}
          </ul>
      </div>
   )
