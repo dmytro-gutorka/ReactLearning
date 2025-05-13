ef733753f774b45b1bbf6bfa3d0f6edf3f49b209
dimagutorka <dgutorka@gmail.com>
2025-05-13 19:25:47 +0300

    Fix prop drilling problem with Component composition

--- use-popcorn/src/App.js
+++ use-popcorn/src/App.js
@@ -54,20 +54,27 @@ export default function App() {
 
     return (
       <>
-        <NavBar movies={movies}/>
-        <Main movies={movies}/>
+          <NavBar>
+              <Logo />
+              <Search />
+              <NumResults movies={movies} />
+          </NavBar>
+          <Main>
+              <ListBox>
+                  <MovieList movies={movies}/>
+              </ListBox>
+              <WatchedBox />
+          </Main>
       </>
   );
 }
 
 
-function NavBar({ movies }) {
+function NavBar({ children }) {
 
   return (
     <nav className="nav-bar">
-        <Logo />
-        <Search />
-        <NumResults movies={movies}/>
+        {children}
     </nav>
   )
 }
@@ -109,18 +116,17 @@ function NumResults({ movies }) {
 }
 
 
-function Main({ movies }) {
+function Main({ children }) {
 
   return (
       <main className="main">
-          <ListBox movies={movies}/>
-          <WatchedBox />
+          {children}
       </main>
   )
 }
 
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
     const [isOpen1, setIsOpen1] = useState(true);
 
     return (
@@ -131,7 +137,7 @@ function ListBox({ movies }) {
             >
                 {isOpen1 ? "–" : "+"}
             </button>
-            {isOpen1 && <MovieList movies={movies}/>}
+            {isOpen1 && children}
         </div>
     )
 }
