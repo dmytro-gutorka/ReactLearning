ccc78d5b92dc51a35a817d079752687896ed74ce
dimagutorka <dgutorka@gmail.com>
2025-05-15 20:07:39 +0300

    Movie detail loading

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import StarRating from "./StarRating";
 
 
 const tempMovieData = [
@@ -59,7 +60,6 @@ export default function App() {
 
                 if (data.Response === 'False') throw new Error('Movie was not found')
 
-                console.log(data)
                 setMovies(data.Search)
                 setIsLoading(false)
             }
@@ -242,14 +242,65 @@ function Movie({ movie, onSelectMovie }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+    const [movie, setMovie] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
+
+    const {
+        Title: title,
+        Year: year,
+        Poster: poster,
+        Runtime: runtime,
+        imdbRating,
+        Plot: plot,
+        Released: released,
+        Actors: actors,
+        Director: director,
+        Genre: genre,
+    } = movie
+
+    useEffect(function() {
+
+        async function getMovieDetails() {
+            const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`)
+            const data = await res.json()
+
+            setMovie(data)
+
+            setIsLoading(false)
+        }
+
+        setIsLoading(true)
+        getMovieDetails()
+
+    }, [selectedId])
 
     return (
         <div className="details">
-            <button className="btn-back" onClick={onCloseMovie}>&larr;</button>
-            {selectedId}
+            {isLoading ? <Loader/> :
+                (<>
+                    <header>
+                        <button className="btn-back" onClick={onCloseMovie}>&larr;</button>
+                        <img src={poster} alt={`Poster of ${title}`}/>
+                        <div className='details-overview'>
+                            <h2>{title}</h2>
+                            <p>{released} &bull; {runtime}</p>
+                            <p>{genre}</p>
+                            <p><span>⭐️</span>{imdbRating} IMDb rating</p>
+                        </div>
+                    </header>
+                    <section>
+                        <div className="rating">
+                            <StarRating maxRating={10} size={24}/>
+                        </div>
+                        <p><em>{plot}</em></p>
+                        <p>Starring {actors}</p>
+                        <p>Directed by {director}</p>
+                    </section>
+                    </>)}
         </div>
     )
 }
+
 
 function WatchedSummary({ watched }) {
     const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
