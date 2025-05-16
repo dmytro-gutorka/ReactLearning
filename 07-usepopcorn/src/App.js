41c2d9c0f67472db14013e66c55e44e5375f14ae
dimagutorka <dgutorka@gmail.com>
2025-05-16 19:34:14 +0300

    Create custom useMovies hook

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -1,46 +1,23 @@
 import {useEffect, useRef, useState} from "react";
 import StarRating from "./StarRating";
+import {useMovies} from "./useMovies";
 
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster: "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
+const KEY = 'd0e93483'
 
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0)
 
 
-const KEY = 'd0e93483'
-
-
 export default function App() {
-    const [movies, setMovies] = useState([]);
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] = useState("")
     const [query, setQuery] = useState("");
     const [selectedId, setSelectedId] = useState(null)
     const [watched, setWatched] = useState(function() {
         return JSON.parse(localStorage.getItem('watched'))
     })
 
+    const {movies, isLoading, error} = useMovies(query)
 
     function handleSelectMovie(id) {
         setSelectedId((selectedId) => selectedId === id ? null : id)
@@ -63,48 +40,6 @@ export default function App() {
 
 
     }, [watched])
-
-    useEffect(function() {
-        const controller = new AbortController();
-
-        async function fetchMovies() {
-
-            try {
-                setIsLoading(true)
-                setError('')
-
-                const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`, {signal: controller.signal})
-
-                if (!res.ok) throw new Error('Something went wrong with fetching movies')
-
-                const data = await res.json()
-
-                if (data.Response === 'False') throw new Error('Movie was not found')
-
-                setMovies(data.Search)
-                setError('')
-            }
-
-            catch (err) {
-                if (err.name !== 'AbortError') setError(err.message)
-            }
-
-            finally {
-                setIsLoading(false)
-            }
-        }
-
-        if (query.length < 3) {
-            setMovies([])
-            setError("")
-            return;
-        }
-
-        fetchMovies();
-
-        return () => controller.abort()
-
-    }, [query])
 
     return (
         <>
@@ -434,7 +369,7 @@ function WatchedMoviesList({ watched, onDeleteWatched }) {
     return (
         <ul className="list">
             {watched.map((movie) => (
-                <WatchedMovie movie={movie} onDeleteWatched={onDeleteWatched}/>
+                <WatchedMovie movie={movie} onDeleteWatched={onDeleteWatched} key={movie.imdbID}/>
                 ))}
         </ul>
     )
