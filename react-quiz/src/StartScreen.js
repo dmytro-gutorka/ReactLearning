e53121ca6dc3f662a35519cecf96cfa6ede3d1c0
dimagutorka <dgutorka@gmail.com>
2025-05-19 17:45:29 +0300

    create Question component

--- react-quiz/src/StartScreen.js
+++ react-quiz/src/StartScreen.js
@@ -1,11 +1,11 @@
-export default function StartScreen({ numQuestions }) {
+export default function StartScreen({ numQuestions, dispatch }) {
 
     return (
         <div className="start">
             <h2>Welcome to The React Quiz1</h2>
             <h3>{numQuestions} questions to test your React mastery</h3>
 
-            <button>Let's start</button>
+            <button onClick={() => dispatch({type: 'start'})}>Let's start</button>
         </div>
     )
 }
