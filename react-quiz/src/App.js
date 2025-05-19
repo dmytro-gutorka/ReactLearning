ce73c0d1ff637e177965cd2749237072bde4fd49
dimagutorka <dgutorka@gmail.com>
2025-05-19 19:19:33 +0300

    Moving to the next question

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -6,6 +6,7 @@ import Loader from "./components/Loader";
 import Error from "./components/Error";
 import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
+import NextButton from "./components/NextButton";
 
 
 const initialState = {
@@ -34,6 +35,8 @@ function reducer(state, action) {
                     ? state.points + question.points
                     : state.points
             }
+        case 'nextQuestion':
+            return { ...state, index: state.index + 1, answer: null }
         default:
             throw new Error('Action unknown')
     }
@@ -58,7 +61,12 @@ export default function App() {
               {status === 'loading' && <Loader/>}
               {status === 'error' && <Error/>}
               {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
-              {status === 'active' && <Question question={questions[index]} dispatch={dispatch} answer={answer}/>}
+              {status === 'active' &&
+                  <>
+                  <Question question={questions[index]} dispatch={dispatch} answer={answer}/>
+                  <NextButton dispatch={dispatch} answer={answer}/>
+                  </>
+              }
           </Main>
       </div>
   )
