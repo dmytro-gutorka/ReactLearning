dff17723a7f17c672837bab653aeb7fa754e9e48
dimagutorka <dgutorka@gmail.com>
2025-05-19 19:01:11 +0300

    Handling new answers

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,18 +1,21 @@
 import {useEffect, useReducer} from "react";
 
-import Header from "./Header";
-import Main from "./Main";
-import Loader from "./Loader";
-import Error from "./Error";
-import StartScreen from "./StartScreen";
-import Question from "./Question";
-
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
 
 
 const initialState = {
     questions: [],
     status: 'loading', // 'loading', 'error', 'ready', 'active', 'finished'
+    index: 0,
+    answer: null,
+    points: 0
 }
+
 
 function reducer(state, action) {
     switch(action.type) {
@@ -22,13 +25,22 @@ function reducer(state, action) {
             return { ...state, status: 'error'}
         case 'start':
             return { ...state, status: 'active'}
+        case 'newAnswer':
+            const question = state.questions.at(state.index)
+            return {
+                ...state, answer: action.payload,
+
+                points: action.payload === question.correctOption
+                    ? state.points + question.points
+                    : state.points
+            }
         default:
             throw new Error('Action unknown')
     }
 }
 
 export default function App() {
-    const [{questions, status}, dispatch] = useReducer(reducer, initialState)
+    const [{questions, status, index, answer}, dispatch] = useReducer(reducer, initialState)
 
     const numQuestions = questions.length
 
@@ -46,7 +58,7 @@ export default function App() {
               {status === 'loading' && <Loader/>}
               {status === 'error' && <Error/>}
               {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
-              {status === 'active' && <Question/>}
+              {status === 'active' && <Question question={questions[index]} dispatch={dispatch} answer={answer}/>}
           </Main>
       </div>
   )
