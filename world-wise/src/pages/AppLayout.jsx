88b622449eee4ccc546c632c7bbdcbfb6327cb36
dimagutorka <dgutorka@gmail.com>
2025-05-22 10:51:54 +0300

    Connect leaflet map + popups

--- world-wise/src/pages/AppLayout.jsx
+++ world-wise/src/pages/AppLayout.jsx
@@ -2,7 +2,6 @@ import Sidebar from "../components/Sidebar";
 import Map from "../components/Map";
 
 import styles from './AppLayout.module.css'
-import {Outlet} from "react-router-dom";
 
 
 export default function AppLayout() {
