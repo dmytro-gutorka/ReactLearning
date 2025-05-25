787168949394f8cc343a618680198327e32a8123
dimagutorka <dgutorka@gmail.com>
2025-05-25 15:15:55 +0300

    add additional reducer

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,16 +1,24 @@
-import { createStore } from 'redux';
+import {combineReducers, createStore} from 'redux';
 
 
-const initialState = {
+const initialStateAccount = {
     balance: 0,
     loan: 0,
     loanPurpose: '',
 }
 
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+    fullName: '',
+    nationalID: '',
+    createdAt: ''
+}
+
+
+function accountReducer(state = initialStateAccount, action) {
 
     switch(action.type) {
+
         case "account/deposit":
             return { ...state, balance: state.balance + action.payload }
         case "account/withdraw":
@@ -20,13 +28,26 @@ function reducer(state = initialState, action) {
             return { ...state, loan: action.payload.amount, loanPurpose: action.payload.purpose }
         case "account/payLoan":
             return { ...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan }
+
         default:
             return state;
     }
 }
 
 
-const store = createStore(reducer)
+function customerReducer(state = initialStateCustomer, action) {
+
+    switch (action.type) {
+
+        case "customer/createCustomer":
+            return { ...state, fullName: action.payload.fullName, nationalID: action.payload.nationalID }
+        case "customer/updateName":
+            return { ...state, fullName: action.payload }
+
+        default:
+            return state
+    }
+}
 
 
 const deposit = (amount) =>  ({ type: 'account/deposit', payload: amount })
@@ -37,10 +58,30 @@ const requestLoan = (amount, purpose) =>  ({ type: 'account/requestLoan', payloa
 
 const payLoan = () =>  ({ type: 'account/payLoan' })
 
+const createCustomer = (fullName, nationalID) => ({
+    type: 'customer/createCustomer',
+    payload: { fullName, nationalID, createdAt: new Date().toISOString()
+    }})
+
+const updateName = (fullName) => ({ type: "customer/updateName", payload: fullName})
+
+
+const rootReducer = combineReducers({
+    account: accountReducer,
+    customer: customerReducer,
+})
+
+
+const store = createStore(rootReducer)
+
 
 store.dispatch(deposit(1000))
 store.dispatch(withdraw(500))
 store.dispatch(requestLoan(5000, 'buy a house'))
 store.dispatch(payLoan())
+
+store.dispatch(createCustomer('Dima', 11112222))
+store.dispatch(updateName('NEdima'))
+
 
 console.log(store.getState())
