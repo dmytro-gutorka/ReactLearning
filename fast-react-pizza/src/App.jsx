41ef4fed30588c81f030e57d40544a98c7d2bb5a
dimagutorka <dgutorka@gmail.com>
2025-05-26 18:47:44 +0300

    Add router to the project

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,7 +1,40 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import Cart from "./features/cart/Cart";
+import Menu from "./features/menu/Menu";
+import Home from "./ui/Home";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />
+    },
+    {
+        path: '/menu',
+        element: <Menu />
+    },
+    {
+        path: '/card',
+        element: <Cart />
+    },
+    {
+        path: '/order/new',
+        element: <CreateOrder />
+    },
+    {
+        path: '/order/:orderId',
+        element: <Order />
+    }
+])
+
+
 function App() {
 
     return (
-        <div>Hello Vite</div>
+        <RouterProvider router={router} />
     )
 }
 
